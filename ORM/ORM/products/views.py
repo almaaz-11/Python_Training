04ac232c5af71ceb4764f1c1e7eeffbe3b06825a
almaaz-11 <almaaz.ahmed@velsof.com>
2025-12-08 00:32:15 -0800

    Day-3 ORM and Session 4

--- ORM/ORM/products/views.py
+++ ORM/ORM/products/views.py
@@ -9,4 +9,23 @@ def visit_count_view(request):
 
     request.session['visit_count'] = visit_count
 
+    request.session['fav_lang'] = 'Python'
+
+    print(request.session.items())
+
     return HttpResponse(f"You have visited this page {visit_count} times")
+
+def remove_favorite_language(request):
+    if 'fav_lang' in request.session:
+        del request.session['fav_lang']
+
+    return HttpResponse("Favorite language removed from session.")
+
+def clear_session(request):
+    request.session.clear()
+    return HttpResponse("All session data cleared.")
+
+def flush_session(request):
+    request.session.flush()
+    return HttpResponse("Session completely flushed.")
+
